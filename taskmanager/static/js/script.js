7309c0f5d86632ffe9fcbe6c56d8d1dcab897bd8
Igor Basuga <igor.basuga@gmail.com>
2022-01-15 15:49:12 +0000

    fix psql issue

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initilization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
